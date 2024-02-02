ac546f2e40fc247a175a14dbabb92cb4060a0e5a
Karan <jaskaran.s@macrew.net>
2024-02-02 18:18:36 +0530

    upload issues

--- GanttChart/build.gradle.kts
+++ GanttChart/build.gradle.kts
@@ -9,7 +9,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.chart.ganttchart"
             artifactId = "gantt-chart"
-            version = " 1.0.8.4"
+            version = " 1.0.8.5"
             afterEvaluate {
                 from(components["release"])
             }
